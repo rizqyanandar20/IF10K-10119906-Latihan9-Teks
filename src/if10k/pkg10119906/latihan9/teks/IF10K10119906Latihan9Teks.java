b47b37967d6923a44dd26a7c148d5bfa8bbc5f21
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2020-10-21 06:58:05 +0700

    first commit

---
+++ src/if10k/pkg10119906/latihan9/teks/IF10K10119906Latihan9Teks.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119906.latihan9.teks;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama      : Rizqy Ananda Rusmana
+ * NIM       : 10119906
+ * Kelas     : IF-10K
+ * Deskripsi : Deklarasi Variabel String.
+ */
+public class IF10K10119906Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        
+        c = 'A';
+        System.out.println("Hasil " + newline + "Char: " + tab + "berisi " + c );
+        
+    }
+    
+}
